8790e6ae433750f0688047c487c7611b5ec2d013
Miran L <miran@neworigin.io>
2018-11-26 12:56:55 +0100

    Updates

---
+++ precache-manifest.94774b9a190a52cb16b49d93bf5b1b52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "be00fc4a29d03016e78b28c9943e3f51",
+    "url": "/drekavac/static/media/tail-spin.be00fc4a.svg"
+  },
+  {
+    "revision": "c9337235f2e0dd4c6030",
+    "url": "/drekavac/static/js/runtime~main.c9337235.js"
+  },
+  {
+    "revision": "c16b75fb88b63fe42655",
+    "url": "/drekavac/static/js/main.c16b75fb.chunk.js"
+  },
+  {
+    "revision": "1efac73b2bddc2a28227",
+    "url": "/drekavac/static/js/1.1efac73b.chunk.js"
+  },
+  {
+    "revision": "c16b75fb88b63fe42655",
+    "url": "/drekavac/static/css/main.ba48954d.chunk.css"
+  },
+  {
+    "revision": "1efac73b2bddc2a28227",
+    "url": "/drekavac/static/css/1.57c3b846.chunk.css"
+  },
+  {
+    "revision": "716676b1bd958987b150f8a51e777afd",
+    "url": "/drekavac/index.html"
+  }
+];
